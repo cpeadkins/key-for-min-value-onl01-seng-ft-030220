9fef1938d1da64c26f73cb9217ea7de941e7cd0e
Charles Adkins <cpeadkins@gmail.com>
2020-03-15 04:43:50 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,11 +8,11 @@ def key_for_min_value(hash)
   lowest_value = 0
 
 
-#   hash.each do |key,value|
-#     if lowest_value == 0 || value < lowest_value
-#       lowest_value = value
-#       lowest_key = key
-#     end
-#   end
-#   lowest_key
-# end
+  hash.each do |key,value|
+    if lowest_value == 0 || value < lowest_value
+      lowest_value = value
+      lowest_key = key
+    end
+  end
+  lowest_key
+end
